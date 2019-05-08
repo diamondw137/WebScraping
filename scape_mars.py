80a872d91511a42226ce960fcf464dde097b60d4
Diamond Williams <45012707+diamondw137@users.noreply.github.com>
2019-05-07 21:16:14 -0500

    updates on scape_mars.py

--- scape_mars.py
+++ scape_mars.py
@@ -1,77 +1,53 @@
-
-#%%
 from splinter import Browser
 from bs4 import BeautifulSoup
 
-
-#%%
+def init_browser():
 executable_path = {'executable_path': 'chromedriver.exe'}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
-#%%
+def scrape_info():
 url = 'https://mars.nasa.gov/news/'
 browser.visit(url)
-
-
-#%%
 html = browser.html
 
 
-#%%
+#scape page into soup
 soup = BeautifulSoup(html, 'html.parser')
 
-
-#%%
-#    Parse HTML with Beautiful Soup
+#  Parse HTML with Beautiful Soup
 # Retrieve all elements that contain book information
 articles = soup.find('li', class_='slide')
-print(articles)
-
-
-#%%
+# print(articles)
 news_title = articles.find(class_='content_title').text
-print(news_title)
-
 news_para = articles.find(class_='article_teaser_body').text
-news_para
 
+news_data = {"new_title": news_title, "new_para":news_para}
 
-#%%
 browser.quit()
 
+return news_data
 
-#%%
+def init_browser():
 executable_path = {'executable_path': 'chromedriver.exe'}
 browser = Browser('chrome', **executable_path, headless=False)
 
-
-#%%
+def scrape_info():
 url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
 browser.visit(url)
 
-
-#%%
+# to be able to select the text to get images
 browser.click_link_by_partial_text('FULL IMAGE')
-
-
-#%%
 browser.click_link_by_partial_text('more info')
 
-
-#%%
+#Parse Beautiful Soup
 html = browser.html
-
-
-#%%
 soup = BeautifulSoup(html, 'html.parser')
 
-
-#%%
+#image retrival
 image_urls = soup.find_all('div', class_='download_tiff')
 
-
-#%%
+#for loop
 for image_url in image_urls:
     if "JPG" in image_url.text:
         featured_image_url = image_url.find('a')['href']
@@ -79,14 +55,14 @@ for image_url in image_urls:
         continue 
 
 
-#%%
+#image print 
 featured_image_url =  'https:'+ featured_image_url
-print(featured_image_url)
+print = print(featured_image_url)
 
-
-#%%
+#quit
 browser.quit()
 
+return print
 
 #%%
 import requests
